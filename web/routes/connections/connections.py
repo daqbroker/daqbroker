5a6bc0de5c874b4f82aa07ba3554513dffb18e4e
António Dias <amcbd89@gmail.com>
2019-10-29 11:17:50 +0000

    Added routes to handle connections to databases in local storage

--- web/routes/connections/connections.py
+++ web/routes/connections/connections.py
@@ -1,9 +1,9 @@
 from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
 from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUND, HTTP_409_CONFLICT, HTTP_422_UNPROCESSABLE_ENTITY
 
-from daqbroker.web.routes.utils import get_current_user, Depends, HTTPException, oauth2_scheme
+from daqbroker.web.routes.utils import get_current_user, make_url, test_connection
 
 from daqbroker.storage import session
 from daqbroker.storage.local_settings import Connection, User
@@ -20,35 +20,36 @@ async def get_users(current_user: User = Depends(get_current_user)):
 
 #The way of making actions over specific connections must be well handled, probably over connection.id
 
-#@app.get("/{connection}", response_model=ConnectionData)
-#async def get_users(username: str, current_user: User = Depends(get_current_user)):
-#	current_user.test_security(level=2)
-#	user = get_local_by_attr(db= session, Resource= User, attr_name= "username", attr_val= username)
-#	#user = User(db= users_db, username= username).get()
-#	if not user:
-#		raise HTTPException(
-#			status_code=HTTP_404_NOT_FOUND,
-#			detail="User " + username + " not found",
-#			headers={"WWW-Authenticate": "Bearer"},
-#		)
-#	return user
+@app.get("/{conn_id}", response_model=ConnectionData)
+async def get_users(conn_id: int, current_user: User = Depends(get_current_user)):
+	current_user.test_security(level=2)
+	connection = get_local_resource(db= session, Resource= Connection, r_id= conn_id)
+	if not connection:
+		raise HTTPException(
+			status_code=HTTP_404_NOT_FOUND,
+			detail="Connection not found",
+			headers={"WWW-Authenticate": "Bearer"},
+		)
+	return connection
 
-#@app.post("/", response_model=ConnectionData)
-#async def add_user(new_user: ConnectionInput, current_user: User = Depends(get_current_user)):
-#	current_user.test_security(level=3)
-#	connection = get_local_by_attr(db= session, Resource= User, attr_name= "username", attr_val= new_user.username)
-#	if user:
-#		raise HTTPException(
-#			status_code=HTTP_409_CONFLICT,
-#			detail="User '" + new_user.username + "' already exists",
-#			headers={"WWW-Authenticate": "Bearer"},
-#		)
-#	return add_local_resource(db= session, Resource= User, user_input= new_user)
+@app.post("/", response_model=ConnectionData)
+async def add_user(new_conn: ConnectionInput, current_user: User = Depends(get_current_user)):
+	current_user.test_security(level=3)
+	#Must test connection here
+	return add_local_resource(db= session, Resource= Connection, user_input= new_conn)
 
-#@app.put("/{username}", response_model=ConnectionData)
-#async def change_user(username: str, new_data : UserInput, current_user: User = Depends(get_current_user)):
-#	current_user.test_security(level=2)
-#	to_check = ["username", "email"]
+@app.put("/{conn_id}", response_model=ConnectionData)
+async def change_user(conn_id: int, new_data : ConnectionInput, current_user: User = Depends(get_current_user)):
+	current_user.test_security(level=3)
+	connection = get_local_resource(db= session, Resource= Connection, r_id= conn_id)
+	exception = HTTPException(
+		status_code=HTTP_404_NOT_FOUND,
+		detail="Connection not found",
+		headers={"WWW-Authenticate": "Bearer"},
+	)
+	if not connection:
+		raise exception
+	return add_local_resource(db= session, Resource= User, user_input= new_data, r_id=user.id)
 #	users = [user for user in get_local_resources(db= session, Resource= User)]
 #	user = get_local_by_attr(db= session, Resource= User, attr_name= "username", attr_val= username)
 #	exception = HTTPException(
